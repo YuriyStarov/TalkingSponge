f2ca41276f06020162cf272cc21f39667e7d2565
Yurk <Starovman@ukr.net>
2023-04-08 18:11:27 +0300

    maskForSleep

--- scripts/bathroom.js
+++ scripts/bathroom.js
@@ -1,7 +1,7 @@
 
 import { collectionBonuses, countCoins, countCrystal, moneyUpdateEvent } from "./collectionAchievements.js";
 import { queue, queueObject } from "./queueObject.js";
-import { allGame, draw, RenderObject, canvasMain, ctxMain, widthCtxMain, heightCtxMain, blinking } from "./rendering.js";
+import { allGame, draw, RenderObject, canvasMain, ctxMain, widthCtxMain, heightCtxMain, blinking, countersIntervals } from "./rendering.js";
 import { coloringMainButton } from "./funcForProgress.js";
 
 export function toilet () {
